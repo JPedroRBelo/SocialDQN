95d85a1cae0a9ab81c3822615dff5825290ad977
José Pedro Belo <josepedroribeirobelo@gmail.com>
2022-01-06 08:12:53 -0300

    updating validation script and config file

--- utils/validate_tool.py
+++ utils/validate_tool.py
@@ -16,10 +16,22 @@ import matplotlib.pyplot as plt
 from config.hyperparams import *
 import pandas as pd
 
+average_scores = []
+wait_average_scores = []
+look_average_scores = []
+wave_average_scores = []
+wave_positive_average_scores = []
+wave_negative_average_scores = []
+handshake_average_scores = []
+handshake_positive_average_scores = []
+handshake_negative_average_scores = []
+handshake_ratio_positive_average_scores = []
+handshake_ratio_negative_average_scores = []
+wave_ratio_positive_average_scores = []
+wave_ratio_negative_average_scores = []
 
 
-
-def plot(scores,name,params,i_episode,save=False):
+def plot(scores,params,i_episode,save=False,title='',metric='Score'):
 
 
 	fig = plt.figure(num=2,figsize=(10, 5))
@@ -42,8 +54,8 @@ def plot(scores,name,params,i_episode,save=False):
 		
 	#plt.fill_between(episode,df['average_scores'].add(df['std']),df['average_scores'].sub(df['std']),alpha=0.3)
 
-	
-	plt.ylabel('Score')
+	plt.title(title)
+	plt.ylabel(metric)
 	plt.xlabel('Episode')
 
 	max_total_fails = -3
@@ -55,13 +67,13 @@ def plot(scores,name,params,i_episode,save=False):
 
 	#ax.set_xticks(major_ticks)
 	#ax.set_xticks(minor_ticks, minor=True)
-	ax.set_yticks(major_ticks)
-	ax.set_yticks(minor_ticks, minor=True)
-	ax.axhline(1, color='gray', linewidth=0.5)
-	ax.axhline(0, color='gray', linewidth=0.5)
-	ax.axhline(-1, color='gray', linewidth=0.5)
+	#ax.set_yticks(major_ticks)
+	#ax.set_yticks(minor_ticks, minor=True)
+	#ax.axhline(1, color='gray', linewidth=0.5)
+	#ax.axhline(0, color='gray', linewidth=0.5)
+	#ax.axhline(-1, color='gray', linewidth=0.5)
 	# And a corresponding grid
-	#ax.grid(which='both')#,axis='y')
+	#ax.grid(which='both')#,axis='y')	
 
 	# Or if you want different settings for the grids:
 	ax.grid(which='minor', alpha=0.2)
@@ -127,6 +139,59 @@ def plot_rewards(scores,name,params,i_episode,save=False):
 	plt.show() # pause a bit so that plots are updated
 
 
+
+def plot_culmulative(scores,name,params,i_episode,save=False):
+
+
+	fig = plt.figure(num=2,figsize=(10, 5))
+	plt.clf()
+
+
+	ax = fig.add_subplot(111)
+	episode = np.arange(len(scores[0][0]))
+	
+	average_scores = []
+	count = 1
+	legends = []
+	for scr,legend in scores:
+		df = pandas.DataFrame(scr,columns=['scores','average_scores','std'])
+		plt.plot(episode,df['average_scores'])
+		plt.fill_between(episode,df['average_scores'].add(df['std']),df['average_scores'].sub(df['std']),alpha=0.3)
+		legends.append(legend)
+		count += 1
+	ax.legend(legends,loc='lower right')
+		
+	#plt.fill_between(episode,df['average_scores'].add(df['std']),df['average_scores'].sub(df['std']),alpha=0.3)
+
+	
+	plt.ylabel('Score')
+	plt.xlabel('Episode')
+
+	max_total_fails = -3
+	max_total_success = 1.5
+
+   
+	major_ticks = np.arange(max_total_fails, max_total_success, 0.5)
+	minor_ticks = np.arange(max_total_fails, max_total_success, 0.1)
+
+	#ax.set_xticks(major_ticks)
+	#ax.set_xticks(minor_ticks, minor=True)
+	ax.set_yticks(major_ticks)
+	ax.set_yticks(minor_ticks, minor=True)
+	ax.axhline(1, color='gray', linewidth=0.5)
+	ax.axhline(0, color='gray', linewidth=0.5)
+	ax.axhline(-1, color='gray', linewidth=0.5)
+	# And a corresponding grid
+	#ax.grid(which='both')#,axis='y')
+
+	# Or if you want different settings for the grids:
+	ax.grid(which='minor', alpha=0.2)
+	ax.grid(which='major', alpha=0.5)
+	if(save):
+		fig.savefig('scores/%s_%s_batch_%d_lr_%.E_trained_%d_episodes.png'% (agent.name,env_name,params['batch_size'],params['learning_rate'],i_episode))   # save the figure to file
+
+	plt.show() # pause a bit so that plots are updated
+
 def calc_average_scores(old_scores,maxlen=100):
 	scores = []								 # list containing scores from each episode
 	scores_window = deque(maxlen=maxlen)   # last (window_size) scores
@@ -146,6 +211,7 @@ def box_plot(results):
 
 
 def calc_all_scores(folders,labels):
+	print('\rLoading...', end="")
 	params = PARAMETERS['SimDRLSR']
 	neutral_reward = params['neutral_reward']
 	hs_success_reward = params['hs_success_reward']
@@ -155,11 +221,13 @@ def calc_all_scores(folders,labels):
 	ep_fail_reward = params['ep_fail_reward']
 	results_dict = {}
 	results_rewards = []
-	average_scores = []
+
+
 	for fol,l in zip(folders,labels):
 		pth = os.path.join(fol,'scores/action_reward_history.dat')
 		rewards = torch.load(pth)
 		results_rewards.append(rewards)
+		v_handshake = []
 		v_hspos = []
 		v_hsneg = []
 		v_wvpos = []
@@ -168,6 +236,9 @@ def calc_all_scores(folders,labels):
 		v_wait  = []
 		v_look  = []
 		ep_durations = []
+
+		scores = []  
+		scores_window = deque(maxlen=100)
 
 		wait_total_scores = []  
 		wait_total_scores_window = deque(maxlen=100)
@@ -187,12 +258,24 @@ def calc_all_scores(folders,labels):
 		handshake_total_scores = []  
 		handshake_total_scores_window = deque(maxlen=100)
 
+		handshake_ratio_positive_scores = []  
+		handshake_ratio_positive_scores_window = deque(maxlen=100)
+
+		handshake_ratio_negative_scores = []  
+		handshake_ratio_negative_scores_window = deque(maxlen=100)
+
 		handshake_positive_scores = []  
 		handshake_positive_scores_window = deque(maxlen=100)
 
 		handshake_negative_scores = []  
 		handshake_negative_scores_window = deque(maxlen=100)
 
+		wave_ratio_negative_scores = []  
+		wave_ratio_negative_scores_window = deque(maxlen=100)
+
+
+		wave_ratio_positive_scores = []  
+		wave_ratio_positive_scores_window = deque(maxlen=100)
 
 
 
@@ -202,6 +285,7 @@ def calc_all_scores(folders,labels):
 
 			hspos = 0
 			hsneg = 0
+			handshake = 0
 			wave = 0
 			wvpos = 0
 			wvneg = 0
@@ -213,6 +297,7 @@ def calc_all_scores(folders,labels):
 					reward = rewards[i][step][1]
 
 					if action == 3 :
+						handshake = handshake=1
 						if reward ==  hs_success_reward:
 							hspos = hspos+1
 						elif reward == hs_fail_reward: 
@@ -260,30 +345,12 @@ def calc_all_scores(folders,labels):
 			v_wvpos.append(wvpos)
 			v_hspos.append(hspos)
 			v_hsneg.append(hsneg)
+			v_handshake.append(handshake)
 
-
-			plot_value = len(rewards[i])
 			len_ep = len(rewards[i])
-			#plot_value = 1	
-			if (arg == 'wv'):
-				plot_value = wave/plot_value
-			elif (arg == 'wv positive'):
-				plot_value = wvpos/plot_value
-			elif (arg == 'wv negative'):
-				plot_value = wvneg/plot_value
-			elif (arg == 'hs'):
-				plot_value = (hspos+hsneg)/plot_value
-			elif (arg == 'hs positive'):
-				plot_value = hspos/plot_value
-			elif (arg == 'hs negative'):
-				plot_value = hsneg/plot_value
-			elif (arg == 'wait'):
-				plot_value = wait/plot_value
-			elif (arg == 'look'):
-				plot_value = look/plot_value
-
+			
 			#wait
-			plot_value = wave/len_ep
+			plot_value = wait/len_ep
 			wait_total_scores_window.append(plot_value) 
 			wait_total_scores.append([plot_value, np.mean(wait_total_scores_window), np.std(wait_total_scores_window)])
 
@@ -293,17 +360,87 @@ def calc_all_scores(folders,labels):
 			look_total_scores.append([plot_value, np.mean(look_total_scores_window), np.std(look_total_scores_window)])
 
 			#wave
-			plot_value = look/len_ep
-			look_total_scores_window.append(plot_value) 
-			look_total_scores.append([plot_value, np.mean(look_total_scores_window), np.std(look_total_scores_window)])
+			plot_value = wave/len_ep
+			wave_total_scores_window.append(plot_value) 
+			wave_total_scores.append([plot_value, np.mean(wave_total_scores_window), np.std(wave_total_scores_window)])
+
+			#wave negative
+			plot_value = wvneg/len_ep
+			wave_negative_scores_window.append(plot_value) 
+			wave_negative_scores.append([plot_value, np.mean(wave_negative_scores_window), np.std(wave_negative_scores_window)])
+
+			#wave positive
+			plot_value = wvpos/len_ep
+			wave_positive_scores_window.append(plot_value) 
+			wave_positive_scores.append([plot_value, np.mean(wave_positive_scores_window), np.std(wave_positive_scores_window)])
+
+			#handshake
+			plot_value = handshake/len_ep
+			handshake_total_scores_window.append(plot_value) 
+			handshake_total_scores.append([plot_value, np.mean(handshake_total_scores_window), np.std(handshake_total_scores_window)])
+
+
+			#handshake positive
+			plot_value = hspos/len_ep
+			handshake_positive_scores_window.append(plot_value) 
+			handshake_positive_scores.append([plot_value, np.mean(handshake_positive_scores_window), np.std(handshake_positive_scores_window)])
+
+			#handshake negative
+			plot_value = hsneg/len_ep
+			handshake_negative_scores_window.append(plot_value) 
+			handshake_negative_scores.append([plot_value, np.mean(handshake_negative_scores_window), np.std(handshake_negative_scores_window)])
+
+			#handshake negative ratio
+			if(hsneg+hspos==0):
+				neg_plot_value = 0
+				pos_plot_value = 0
+			else:
+				neg_plot_value = hsneg/(hsneg+hspos)
+				pos_plot_value = hspos/(hsneg+hspos)
+
+			handshake_ratio_negative_scores_window.append(neg_plot_value) 
+			handshake_ratio_negative_scores.append([neg_plot_value, np.mean(handshake_ratio_negative_scores_window), np.std(handshake_ratio_negative_scores_window)])
+
+			#handshake positive ratio
+			handshake_ratio_positive_scores_window.append(pos_plot_value) 
+			handshake_ratio_positive_scores.append([pos_plot_value, np.mean(handshake_ratio_positive_scores_window), np.std(handshake_ratio_positive_scores_window)])
+
+			if(wvneg+wvpos==0):
+				neg_plot_value = 0
+				pos_plot_value = 0
+			else:
+				neg_plot_value = wvneg/(wvneg+wvpos)
+				pos_plot_value = wvpos/(wvneg+wvpos)
+
+			wave_ratio_negative_scores_window.append(neg_plot_value) 
+			wave_ratio_negative_scores.append([neg_plot_value, np.mean(wave_ratio_negative_scores_window), np.std(wave_ratio_negative_scores_window)])
+
+			#handshake positive ratio
+			wave_ratio_positive_scores_window.append(pos_plot_value) 
+			wave_ratio_positive_scores.append([pos_plot_value, np.mean(wave_ratio_positive_scores_window), np.std(wave_ratio_positive_scores_window)])
 			
 
-		
+			#plot_value = len(rewards[i])
+			#scores_window.append(plot_value)
+			#scores.append([plot_value, np.mean(scores_window), np.std(scores_window)])
 
-			scores_window.append(plot_value)
-			scores.append([plot_value, np.mean(scores_window), np.std(scores_window)])
+		print('\rCalculating...', end="")
+		#average_scores.append([calc_average_scores(scores,maxlen=100),l])
+		wait_average_scores.append([calc_average_scores(wait_total_scores,maxlen=100),l])
+		look_average_scores.append([calc_average_scores(look_total_scores,maxlen=100),l])
+		wave_average_scores.append([calc_average_scores(wave_total_scores,maxlen=100),l])
+		wave_positive_average_scores.append([calc_average_scores(wave_positive_scores,maxlen=100),l])
+		wave_negative_average_scores.append([calc_average_scores(wave_negative_scores,maxlen=100),l])
+		handshake_average_scores.append([calc_average_scores(handshake_total_scores,maxlen=100),l])
+		handshake_positive_average_scores.append([calc_average_scores(handshake_positive_scores,maxlen=100),l])
+		handshake_negative_average_scores.append([calc_average_scores(handshake_negative_scores,maxlen=100),l])
+		handshake_ratio_positive_average_scores.append([calc_average_scores(handshake_ratio_positive_scores,maxlen=100),l])
+		handshake_ratio_negative_average_scores.append([calc_average_scores(handshake_ratio_negative_scores,maxlen=100),l])
+		wave_ratio_positive_average_scores.append([calc_average_scores(wave_ratio_positive_scores,maxlen=100),l])
+		wave_ratio_negative_average_scores.append([calc_average_scores(wave_ratio_negative_scores,maxlen=100),l])
 
-		average_scores.append([calc_average_scores(scores,maxlen=100),l])
+	print('\rLoaded!\t', end="")
+	print('\n')
 
 
 def compare_actions(folders,labels,arg):
@@ -334,8 +471,7 @@ def compare_actions(folders,labels,arg):
 		scores = []  
 		scores_window = deque(maxlen=100)
 
-		for i in range(len(rewards)):
-			
+		for i in range(len(rewards)):			
 
 			hspos = 0
 			hsneg = 0
@@ -449,7 +585,7 @@ def compare_cumulative_rewards(folders,labels):
 	
 	#recalc_scores2 = calc_average_scores(scores['scores'],maxlen=500)
 	#box_plot(average_scores)
-	plot(scores,'result',params,100)
+	plot_culmulative(scores,'result',params,100)
 
 def main():
 	folders = []
@@ -463,12 +599,13 @@ def main():
 	#folders.append('results/20211103_084843')
 
 
-	labels.append('Gray and Face States')
+	#labels.append('Gray and Face States')
+	labels.append("'Visible Face' States")
 	folders.append('results/20211115_185645')
 
 	
-	labels.append('Only Gray State')
-	folders.append('results/20211107_055205')
+	#labels.append('Only Gray State')
+	#folders.append('results/20211107_055205')
 
 	
 	
@@ -484,32 +621,68 @@ def main():
 	#labels.append('Only Depth State')
 	#folders.append('results/20211202_230251')
 
-	
-	
+	labels.append("'Emotions' States'")
+	#75k rb
+	folders.append('results/20220103_083937')
+	#50k rb 20211229_034231
+	labels.append("Emotions 50k")
+	folders.append('results/20211229_034231')
+
 
 	
 	
+
+	calc_all_scores(folders,labels)
+	params = PARAMETERS['SimDRLSR']
 
 	while True:
 		print("=========================")		
 		print("|1 :Culmulative Rewards\t|")
-		print("|2 :Wave\t\t|")
-		print("|3 :Wave Positive\t|")
-		print("|4 :Wave Negative\t|")
-		print("|X :Exit\t\t|")
+		print("|2 :Wait\t\t|")
+		print("|3 :Look\t\t|")
+		print("|4 :Wave\t\t|")
+		print("|5 :Wave Positive\t|")
+		print("|6 :Wave Negative\t|")
+		print("|7 :Handshake\t\t|")
+		print("|8 :Handshake Pos\t|")
+		print("|9 :Handshake Neg\t|")
+		print("|W :Ratio HS Pos\t|")
+		print("|E :Ratio HS Neg\t|")
+		print("|R :Ratio Wv Pos\t|")
+		print("|T :Ratio Wv Neg\t|")
+		print("|0 :Exit\t\t|")
 		print("=========================")
 		with keyboard.Events() as events:
 			# Block for as much as possible
 			event = events.get(1e6)
 			if event.key == keyboard.KeyCode.from_char('1'):
 				compare_cumulative_rewards(folders,labels)
-			elif event.key == keyboard.KeyCode.from_char('2'):
-				compare_actions(folders,labels,'wv')
-			elif event.key == keyboard.KeyCode.from_char('3'):
-				compare_actions(folders,labels,'wv positive')
 			elif event.key == keyboard.KeyCode.from_char('4'):
-				compare_actions(folders,labels,'wv negative')
-			elif event.key == keyboard.KeyCode.from_char('x'):
+				plot(wave_average_scores,params,100,title='Wave',metric='Ratio (%)')
+			elif event.key == keyboard.KeyCode.from_char('5'):
+				plot(wave_positive_average_scores,params,100,title='Wave with positive Reward',metric='Ratio (%)')
+			elif event.key == keyboard.KeyCode.from_char('6'):
+				plot(wave_negative_average_scores,params,100,title='Wave with negative Reward',metric='Ratio (%)')
+			elif event.key == keyboard.KeyCode.from_char('7'):
+				plot(handshake_average_scores,params,100,title='Handshake',metric='Ratio (%)')
+			elif event.key == keyboard.KeyCode.from_char('8'):
+				plot(handshake_positive_average_scores,params,100,title='Handshake with positive Reward',metric='Ratio (%)')
+			elif event.key == keyboard.KeyCode.from_char('9'):
+				plot(handshake_negative_average_scores,params,100,title='Handshake with negative Reward',metric='Ratio (%)')
+			elif event.key == keyboard.KeyCode.from_char('w'):
+				plot(handshake_ratio_positive_average_scores,params,100,title='Ratio Positive Handshake',metric='Ratio (%)')
+			elif event.key == keyboard.KeyCode.from_char('e'):
+				plot(handshake_ratio_negative_average_scores,params,100,title='Ratio Negative Handshake',metric='Ratio (%)')
+			elif event.key == keyboard.KeyCode.from_char('r'):
+				plot(wave_ratio_positive_average_scores,params,100,title='Ratio Positive Wave',metric='Ratio (%)')
+			elif event.key == keyboard.KeyCode.from_char('t'):
+				plot(wave_ratio_negative_average_scores,params,100,title='Ratio Negative Wave',metric='Ratio (%)')
+
+			elif event.key == keyboard.KeyCode.from_char('3'):
+				plot(look_average_scores,params,100,title='Look',metric='Ratio (%)')
+			elif event.key == keyboard.KeyCode.from_char('2'):
+				plot(wait_average_scores,params,100,title='Wait',metric='Ratio (%)')
+			elif event.key == keyboard.KeyCode.from_char('0'):
 				break;			
 			else:
 				print('\nIncorrect key...')
