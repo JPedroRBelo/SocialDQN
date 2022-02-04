a2aa232abe18a4a051b580ab69ea6a40ca1ca72d
José Pedro Belo <josepedroribeirobelo@gmail.com>
2022-02-04 11:23:06 -0300

    updating

--- config/hyperparams.py
+++ config/hyperparams.py
@@ -5,7 +5,7 @@ PARAMETERS = {
     'SimDRLSR': {
         'env_name':             "simDRLSR",
         'simulation_speed':     10,
-        'number_of_agents':     15,
+        'number_of_agents':     10,
         'action_size':          4,
         'state_size':           8,
         't_steps':              10,
