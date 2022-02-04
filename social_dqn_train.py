dd1abdaae25210d81de416a953f823023e66e5e6
José Pedro Belo <josepedroribeirobelo@gmail.com>
2022-02-04 15:58:27 -0300

    updating

--- social_dqn_train.py
+++ social_dqn_train.py
@@ -352,24 +352,24 @@ def execute_ep(env,agent,i_episode,memory,params,epsilon,scores,scores_window,ac
         #env.reset()
 
         # Capture the current state
-        if(debug && ep_debug==i_episode) print("Line: "+str(getframeinfo(currentframe()).lineno))
+        if(debug and ep_debug==i_episode): print("Line: "+str(getframeinfo(currentframe()).lineno))
         gray_state,depth_state = env.get_screen()
-        if(debug && ep_debug==i_episode) print("Line: "+str(getframeinfo(currentframe()).lineno))
+        if(debug and ep_debug==i_episode): print("Line: "+str(getframeinfo(currentframe()).lineno))
         # Reset score collector
         score = 0
         done = False
         # One episode loop
         step = 0
         while not done:
-            if(debug && ep_debug==i_episode) print("Line: "+str(getframeinfo(currentframe()).lineno))
+            if(debug and ep_debug==i_episode): print("Line: "+str(getframeinfo(currentframe()).lineno))
             # Action selection by Epsilon-Greedy policy
             action = agent.eGreedy(gray_state,epsilon)
             #action = agent.select_action(gray_state,depth_state)
-            if(debug && ep_debug==i_episode) print("Line: "+str(getframeinfo(currentframe()).lineno))
+            if(debug and ep_debug==i_episode): print("Line: "+str(getframeinfo(currentframe()).lineno))
             reward, done = env.execute(action)
-            if(debug && ep_debug==i_episode) print("Line: "+str(getframeinfo(currentframe()).lineno))
+            if(debug and ep_debug==i_episode): print("Line: "+str(getframeinfo(currentframe()).lineno))
             next_gray_state,next_depth_state = env.get_screen()
-            if(debug && ep_debug==i_episode) print("Line: "+str(getframeinfo(currentframe()).lineno))
+            if(debug and ep_debug==i_episode): print("Line: "+str(getframeinfo(currentframe()).lineno))
 
             if(save_images):
                 if(next_gray_state!=None):
@@ -381,14 +381,14 @@ def execute_ep(env,agent,i_episode,memory,params,epsilon,scores,scores_window,ac
 
             # Store experience
             memory.push(gray_state, action, reward, next_gray_state, done)
-            if(debug && ep_debug==i_episode) print("Line: "+str(getframeinfo(currentframe()).lineno))
+            if(debug and ep_debug==i_episode): print("Line: "+str(getframeinfo(currentframe()).lineno))
             ep_actions_rewards.append([action,reward])
-            if(debug && ep_debug==i_episode) print("Line: "+str(getframeinfo(currentframe()).lineno))
+            if(debug and ep_debug==i_episode): print("Line: "+str(getframeinfo(currentframe()).lineno))
             if(save_social_states):
                 ep_social_state.append(gray_state[1])
             # Update Q-Learning
             step += 1
-            if(debug && ep_debug==i_episode) print("Line: "+str(getframeinfo(currentframe()).lineno))
+            if(debug and ep_debug==i_episode): print("Line: "+str(getframeinfo(currentframe()).lineno))
             if (not train_after_episodes) and (step % update_interval) == 0 and len(memory) > replay_start:
                 # Recall experiences (miniBatch)
                 experiences = memory.recall()
@@ -397,20 +397,20 @@ def execute_ep(env,agent,i_episode,memory,params,epsilon,scores,scores_window,ac
                 #print('\r#Training step:{}'.format(step), end="")
 
             # State transition
-            if(debug && ep_debug==i_episode) print("Line: "+str(getframeinfo(currentframe()).lineno))
+            if(debug and ep_debug==i_episode): print("Line: "+str(getframeinfo(currentframe()).lineno))
             gray_state = next_gray_state
             depth_state = next_depth_state
 
             # Update total score
             score += reward
-        if(debug && ep_debug==i_episode) print("Line: "+str(getframeinfo(currentframe()).lineno))
+        if(debug and ep_debug==i_episode): print("Line: "+str(getframeinfo(currentframe()).lineno))
 
         actions_rewards[i_episode] =  ep_actions_rewards
         if(save_social_states):
             social_signals[i_episode] =  ep_social_state
         scores_window.append(score)
         scores[i_episode] = [score, np.mean(scores_window), np.std(scores_window)]
-        if(debug && ep_debug==i_episode) print("Line: "+str(getframeinfo(currentframe()).lineno))
+        if(debug and ep_debug==i_episode): print("Line: "+str(getframeinfo(currentframe()).lineno))
         # Print episode summary
         print('\r#TRAIN Episode:{}, Score:{:.2f}, Average Score:{:.2f}, Exploration:{:1.4f}'.format(i_episode, score, np.mean(scores_window), epsilon), end="")
         #if i_episode % 100 == 0:
