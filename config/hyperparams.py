a397b39aa6d892f27adb68093365eefb71520764
José Pedro Belo <josepedroribeirobelo@gmail.com>
2022-03-02 16:43:26 -0300

    multimodal support: depth images

--- config/hyperparams.py
+++ config/hyperparams.py
@@ -4,7 +4,7 @@
 PARAMETERS = {
     'SimDRLSR': {
         'env_name':             "simDRLSR",
-        'simulation_speed':     5,
+        'simulation_speed':     2,
         'number_of_agents':     5,
         'action_size':          4,
         'state_size':           8,
