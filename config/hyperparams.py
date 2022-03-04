11fbbfd52cd1e3262a10d07c67f322dcbe73ee47
José Pedro Belo <josepedroribeirobelo@gmail.com>
2022-03-04 10:00:23 -0300

    updating waiting max time

--- config/hyperparams.py
+++ config/hyperparams.py
@@ -5,7 +5,7 @@ PARAMETERS = {
     'SimDRLSR': {
         'env_name':             "simDRLSR",
         'simulation_speed':     1,
-        'number_of_agents':     15,
+        'number_of_agents':     8,
         'action_size':          4,
         'state_size':           8,
         't_steps':              25,
